3447e0cf4ccd2b3f07fa1f589484021606e7533a
luna-ortus-cor <100546671+luna-ortus-cor@users.noreply.github.com>
2022-11-28 23:36:49 +0800

    Update sorting.cpp

--- sorting.cpp
+++ sorting.cpp
@@ -133,5 +133,35 @@ void countingSort(vector<int> (&a)){
 }
 
 // O(N) for large range but few digits
-void radixSort(vector<int> (&a)){
+int getMax(int arr[], int size) { 
+    int max = arr[0]; 
+    for (int i = 1; i < size; i++) 
+        if (arr[i] > max) 
+            max = arr[i]; 
+    return max; 
+} 
+  
+void CountingSort(int arr[], int size, int div) { 
+    int output[size]; 
+    int count[10] = {0}; 
+    for (int i = 0; i < size; i++) {
+        count[ (arr[i]/div)%10 ]++; 
+    }
+    for (int i = 1; i < 10; i++) {
+        count[i] += count[i - 1]; 
+    }
+    for (int i = size - 1; i >= 0; i--) { 
+        output[count[ (arr[i]/div)%10 ] - 1] = arr[i]; 
+        count[ (arr[i]/div)%10 ]--; 
+    }
+    for (int i = 0; i < size; i++) {
+        arr[i] = output[i]; 
+    }
+} 
+  
+void RadixSort(int arr[], int size) { 
+    int m = getMax(arr, size); 
+    for (int div = 1; m/div > 0; div *= 10) 
+        CountingSort(arr, size, div); 
 }
+
