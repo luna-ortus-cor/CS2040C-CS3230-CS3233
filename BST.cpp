ba8041f1d8d5fc8e211a7ac059e3d2c102ae541b
luna-ortus-cor <100546671+luna-ortus-cor@users.noreply.github.com>
2022-12-14 17:15:07 +0800

    Create BST.cpp

---
+++ BST.cpp
@@ -0,0 +1,5 @@
+//binary search tree is where every node to the left is smaller than parent, and every node to the right is larger than parent
+//for equal values, need to tweak
+//implements Table or Map Abstract Data Type (ADT)
+//needs to support following 3 operations as efficiently as possible
+//exists(v),insert(v),remove(v)
