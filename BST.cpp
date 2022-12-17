77067626b8ac5afd1a67373c9543e681a437e471
luna-ortus-cor <100546671+luna-ortus-cor@users.noreply.github.com>
2022-12-17 19:25:46 +0800

    Update BST.cpp

--- BST.cpp
+++ BST.cpp
@@ -6,3 +6,8 @@
 //if use unsorted array/vector, O(N), O(1), O(N) time complexity respectively
 //if use sorted array/vector, O(logN), O(N), O(N) time complexity respectively
 //other operations possible: min ele, max ele, next larger ele, previous smaller ele, list ele in sorted
+//consists of root vertex, internal vertices, and leaf vertex (vertices)
+//each vertex at least 4 attributes: parent (can be null), left, right, key/value/data
+//all vertices on left subtree strictly smaller than parent, and all vertices on right subtree strictly greater than parent
+//to handle duplicate, can include additional data to store frequency/occurence of value
+//5. BST operations //TODO + REMOVE
