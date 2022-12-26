594dc2b038eec3463cdf0ef5a547b7022e69a215
luna-ortus-cor <100546671+luna-ortus-cor@users.noreply.github.com>
2022-12-26 17:31:49 +0800

    Update BST.cpp

--- BST.cpp
+++ BST.cpp
@@ -10,4 +10,63 @@
 //each vertex at least 4 attributes: parent (can be null), left, right, key/value/data
 //all vertices on left subtree strictly smaller than parent, and all vertices on right subtree strictly greater than parent
 //to handle duplicate, can include additional data to store frequency/occurence of value
-//5. BST operations //TODO + REMOVE
+//static data structure: only efficient if no (or rare) inserts/deletes/updates
+//dynamic data structure: efficient even if many inserts/deletes/updates
+
+struct Vertex { 
+  int data; 
+  Vertex* parent;
+  Vertex* left;
+  Vertex* right;
+};
+
+class BST:public Vertex{
+private:
+  Vertex *root;
+public:
+  BST(){
+    root=NULL;
+  }
+  Vertex* Search(Vertex* r, int v);
+  int findMin(vertex* r);
+  int findMax(Vertex* r);
+}
+
+// O(h)
+Vertex* BST::Search(Vertex* r, int v){
+  if(r==NULL){
+    return NULL;
+  }else if(v<r->data){
+    return Search(r->left,v);
+  }else if(v>r->data){
+    return Search(r->right,v);
+  }else{
+    return r;
+  }
+}
+
+// O(h)
+int BST::findMin(Vertex* r){
+  while(r!=NULL){
+    if(r->left==NULL){
+      return r->data;
+    }else{
+      return findMin(r->left);
+    }
+  }
+  return NULL;
+}
+
+// O(h)
+int BST::findMax(Vertex* r){
+  while(r!=NULL){
+    if(r->right==NULL){
+      return r->data;
+    }else{
+      return findMax(r->right);
+    }
+  }
+  return NULL;
+}
+
+  
