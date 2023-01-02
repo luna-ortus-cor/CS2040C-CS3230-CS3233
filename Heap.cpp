5fbfc0a81e1a0634787fb79055956ebb71e46961
luna-ortus-cor <100546671+luna-ortus-cor@users.noreply.github.com>
2023-01-02 20:29:29 +0800

    Update Heap.cpp

--- Heap.cpp
+++ Heap.cpp
@@ -7,3 +7,9 @@
 // Dequeue(): return an existing element that has the highest priority in the PQ
 // if tied, can choose to return any, or return in order that was enqueued (FIFO)
 // we can implement PQ using (circular) array or linked list, but this is slow O(N) enqueue and dequeue operation
+// max height of binary heap is log N for N elements
+// store elements in compact array, starting with root at index 1, and subsequent levels from left to right in increasing indices
+// then tree traversal can be simplified to following operations
+// parent(i) = i>>1 (index i divided by 2, integer division)
+// left(i) = i<<1 (index i multiplied by 2)
+// right(i) = (i<<1)+1 (index i multiplied by 2 add 1)
