3425041ba3779084a863617105d213a7a2ff48aa
luna-ortus-cor <100546671+luna-ortus-cor@users.noreply.github.com>
2023-01-03 19:36:04 +0800

    Update Heap.cpp

--- Heap.cpp
+++ Heap.cpp
@@ -13,3 +13,90 @@
 // parent(i) = i>>1 (index i divided by 2, integer division)
 // left(i) = i<<1 (index i multiplied by 2)
 // right(i) = (i<<1)+1 (index i multiplied by 2 add 1)
+// similar to BST, 
+// insert(v) is O(logN)
+// delete(i) is O(logN)
+// update(i,v) is O(logN)
+// we can naively create a binary heap in O(NlogN) by doing N insert(v)
+// alternatively, we notice that by taking the array of numbers as the initial binary heap, 
+// we only need to correct half of the elements (leaves are considered to satisfy the heap property initially, so no change required)
+// 
+
+void Heap::slowCreate(vector<int> vi){
+  for(int v:vi){
+    insert(vi);
+  }
+}
+
+void Heap::fastCreate(vector<int> vi){
+  for(int i=1;i<1+vi.length;i++){
+    A[i]=vi[i-1];
+  }
+  for(int j=(A.length-1)/2;j>=1;j--){
+    int i=j;
+    while(i<A.length){
+      if(A[left(i)]>A[right(i)]){
+        if(A[i]<A[left(i)]){
+          swap(A[i],A[left(i)]);
+          i=left(i);
+        }
+      }else{
+        if(A[i]<A[right(i)]){
+          swap(A[i],A[right(i)]);
+          i=right(i);
+        }
+      }
+    }
+  }
+}
+
+int Heap::parent(int i){
+  return i>>1;
+}
+
+int Heap::left(int i){
+  return i<<1;
+}
+
+int Heap::right(int i){
+  return (i<<1)+1;
+}
+
+void Heap::insert(int v){
+  A[A.length] = v;
+  i = A.length-1;
+  while((i>1)&&(A[i]>A[parent(i)])){
+    swap(A[i],A[parent(i)]);
+    i=parent(i);
+  }
+}
+
+void Heap::delete(int i){
+  A[i]=A[1]+1;
+  while((i>1)&&(A[i]>A[parent(i)])){
+    swap(A[i],A[parent(i)]);
+    i=parent(i);
+  }
+  extract();
+}
+
+int Heap::extract(){ //returns max element for binary max heap, we can easily write similar code to return min element for binary min heap
+  int ret = A[1];
+  A[1]=A[A.length-1];
+  int i=1;
+  A.length--;
+  while(i<A.length){
+    if(A[left(i)]>A[right(i)]){
+      if(A[i]<A[left(i)]){
+        swap(A[i],A[left(i)]);
+        i=left(i);
+      }
+    }else{
+      if(A[i]<A[right(i)]){
+        swap(A[i],A[right(i)]);
+        i=right(i);
+      }
+    }
+  }
+  return ret;
+}
