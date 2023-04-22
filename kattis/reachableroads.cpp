a32bbbd43633df06466f2bd919ee82028b6c3b3e
luna-ortus-cor <100546671+luna-ortus-cor@users.noreply.github.com>
2023-04-22 23:35:28 +0800

    Update reachableroads.cpp

--- kattis/reachableroads.cpp
+++ kattis/reachableroads.cpp
@@ -56,3 +56,52 @@ int main() {
         cout << extras << endl;
     }
 }
+
+//above uses UFDS method
+//below is BFS method to count number of CC and -1
+/*
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    for(int i = 0;i<n;i++){
+        int m;
+        cin>>m;
+        vector<int> visited(m,0);
+        vector<vector<int>> AL(m,vector<int>());
+        int r;
+        cin>>r;
+        for(int j=0;j<r;j++){
+            int a,b;
+            cin>>a>>b;
+            AL[a].push_back(b);
+            AL[b].push_back(a);
+        }
+        int count = 0;
+        for(int k = 0;k<m;k++){
+            if(visited[k]==0){
+                count++;
+            }
+            queue<int> q;
+            q.push(k);
+            while(!q.empty()){
+                int idx = q.front();
+                q.pop();
+                if(visited[idx]==1){
+                    continue;
+                }else{
+                    visited[idx]=1;
+                }
+                for(int c:AL[idx]){
+                    if(visited[c]==0){
+                        q.push(c);
+                    }
+                }
+            }
+        }
+        cout<<count-1<<endl;
+    }
+    return 0;
+}
+*/
