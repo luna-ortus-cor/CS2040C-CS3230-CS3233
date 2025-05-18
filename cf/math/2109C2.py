52efd37247077b81343deaea9c41546cded85b0a
luna-ortus-cor <100546671+luna-ortus-cor@users.noreply.github.com>
2025-05-18 22:31:34 +0800

    Create 2109C2.py

---
+++ cf/math/2109C2.py
@@ -0,0 +1,24 @@
+import sys
+
+#again, number theory
+#by taking the digit sum of 9n twice, we can guarantee result of 9
+#then just add n-9 to get n
+
+t=int(sys.stdin.readline())
+for _ in range(t):
+    n=int(sys.stdin.readline())
+    print('mul 9')
+    sys.stdout.flush()
+    sys.stdin.readline()
+    print('digit')
+    sys.stdout.flush()
+    sys.stdin.readline()
+    print('digit')
+    sys.stdout.flush()
+    sys.stdin.readline()
+    print(f'add {n-9}')
+    sys.stdout.flush()
+    sys.stdin.readline()
+    print('!')
+    sys.stdout.flush()
+    sys.stdin.readline()
